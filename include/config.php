820e02555b6c0870703a1080e96370edc1b45c82
LeoRanoe <lranoesendjojo@gmail.com>
2025-05-23 06:15:58 -0300

    refactor: Standard, DB and User name

--- include/config.php
+++ include/config.php
@@ -12,8 +12,8 @@ define('BASE_URL', 'http://localhost/E-Stem_Suriname');
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'e-stem_suriname');
-define('DB_USER', 'admin');
-define('DB_PASS', 'root');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 // Application configuration
 define('SITE_NAME', 'E-Stem Suriname');
